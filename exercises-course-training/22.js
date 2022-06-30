abc08086800ab338a809d83479c38b9aa0e7b3b1
PF82 <pedrotcfernandes@gmail.com>
2022-07-01 00:47:25 +0100

    21-25

--- exercises-course-training/22.js
+++ exercises-course-training/22.js
@@ -17,9 +17,9 @@ let b = 9;
 let c = 3;
 
 // Only change code below this line
-a = a - 6;
-b = b - 15;
-c = c - 1;
+a -= 6;
+b -= 15;
+c -= 1;
 
 
 console.log(a)
--- exercises-course-training/23.js
+++ exercises-course-training/23.js
@@ -17,9 +17,9 @@ let b = 12;
 let c = 4.6;
 
 // Only change code below this line
-a = a * 5;
-b = 3 * b;
-c = c * 10;
+a *= 5;
+b *= 3;
+c *= 10;
 
 
 console.log(a)
