d7f3a886a0ea91c034ce0f7c1c89ea8122a4af34
PF82 <pedrotcfernandes@gmail.com>
2022-07-01 03:44:50 +0100

    96-100

--- exercises-course-training/100.js
+++ exercises-course-training/100.js
@@ -22,7 +22,10 @@ Declare and initialize a variable total to 0. Use a for loop to add the value of
 const myArr = [2, 3, 4, 5, 6];
 
 // Only change code below this line
-
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+   total += myArr[i];
+}
 
 console.log(total)
 document.getElementById("demo").innerHTML = "";
