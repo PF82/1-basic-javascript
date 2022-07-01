d2d82e124187c049277773d71ef4a2d21e5c43c8
PF82 <pedrotcfernandes@gmail.com>
2022-07-01 04:12:59 +0100

    101-103

--- exercises-course-training/111.js
+++ exercises-course-training/111.js
@@ -38,7 +38,9 @@ check if a number is positive, negative or zero. The function should return posi
 
 
 function checkSign(num) {
-
+  return (num > 0) ? "positive"
+    : (num < 0) ? "negative"
+      : "zero";
 }
 
 checkSign(10);
--- exercises-course-training/112.js
+++ exercises-course-training/112.js
@@ -38,7 +38,13 @@ Your function must use recursion by calling itself and must not use loops of any
 
 // Only change code below this line
 function countdown(n) {
-  return;
+  if (n < 1) {
+    return [];
+  } else {
+    const countArray = countdown(n - 1);
+    countArray.unshift(n);
+    return countArray;
+  }
 }
 // Only change code above this line
 
