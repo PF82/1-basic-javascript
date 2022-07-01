c878f51d1c3f2173ba1c58f507114d2e7c3000ad
PF82 <pedrotcfernandes@gmail.com>
2022-07-01 01:18:25 +0100

    46-50

--- exercises-course-training/47.js
+++ exercises-course-training/47.js
@@ -21,6 +21,7 @@ Add ["Paul", 35] to the beginning of the myArray variable using unshift().
 // Setup
 const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
+myArray.unshift(["Paul", 35]);
 
 // Only change code below this line
 
