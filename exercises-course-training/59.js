db31492ddeb864c527f0652db07cc1f6be77941b
PF82 <pedrotcfernandes@gmail.com>
2022-07-01 01:37:34 +0100

    56-60

--- exercises-course-training/59.js
+++ exercises-course-training/59.js
@@ -37,10 +37,13 @@ was false otherwise.
 
 
 function trueOrFalse(wasThatTrue) {
-    // Only change code below this line
-   
+  // Only change code below this line
 
-    // Only change code above this line
+  if (trueOrFalse) {
+    return "Yes, that was true";
+  }
+  return "No, that was false";
+  // Only change code above this line
 }
 
 
