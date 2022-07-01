ebc088918b9288ed0ed5a4b80f0b7ca56a761120
PF82 <pedrotcfernandes@gmail.com>
2022-07-01 01:59:07 +0100

    71-75

--- exercises-course-training/71.js
+++ exercises-course-training/71.js
@@ -14,19 +14,17 @@ Combine the if statements into a single if/else statement.
 
 
 function testElse(val) {
-    let result = "";
-    // Only change code below this line
+  let result = "";
+  // Only change code below this line
 
-    if (val) {
-        result = "Bigger than 5";
-    }
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
 
-    if (val) {
-        result = "5 or Smaller";
-    }
-
-    // Only change code above this line
-    return result;
+  // Only change code above this line
+  return result;
 }
 
 testElse(4);
--- exercises-course-training/74.js
+++ exercises-course-training/74.js
@@ -24,11 +24,19 @@ num >= 20 - return Huge
 
 
 function testSize(num) {
-    // Only change code below this line
-
-
-    return "Change Me";
-    // Only change code above this line
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else {
+    return "Huge";
+  }
+  // Only change code above this line
 }
 
 testSize(7);
